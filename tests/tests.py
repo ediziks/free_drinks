6a55919ca8998367695d0d2999fec3bd0f8582f6
zx <eemektas@gmail.com>
2023-03-20 00:57:35 +0100

    enhancement: extended test cases

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,7 @@
-import unittest
-from datetime import time
 import os
 import sys
+import unittest
+from datetime import time
 from freezegun import freeze_time
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -11,8 +11,13 @@ from free_drinks.app import FreeDrinks
 
 class TestFreeDrinks(unittest.TestCase):
     def setUp(self):
-        # Create a FreeDrinks object with a valid config string
+        # Creates a FreeDrinks object with a valid config string
         self.fd = FreeDrinks("Mon: 1200-1400 Tue: 0900-1100 Fri: 0000-2400")
+        self.fd_case2 = FreeDrinks(
+            "Mon: 1200-1400 Tue: 0900-1100 Fri: 2200-2400 Fri: 2000-2200"
+        )
+        self.fd_case3 = FreeDrinks("")
+        self.fd_case4 = FreeDrinks("illegal input")
 
     def test_is_valid_day(self):
         self.assertTrue(self.fd.is_valid_day("Mon"))
@@ -49,11 +54,27 @@ class TestFreeDrinks(unittest.TestCase):
 
     def test_parse_config_string(self):
         expected = {
-            "mon": (time(12, 0), time(14, 0)),
-            "tue": (time(9, 0), time(11, 0)),
-            "fri": (time(0, 0), time(23, 59, 59, 999999)),
+            "mon": [(time(12, 0), time(14, 0))],
+            "tue": [(time(9, 0), time(11, 0))],
+            "fri": [(time(0, 0), time(23, 59, 59, 999999))],
         }
         self.assertEqual(self.fd.parse_config_string(), expected)
+
+        expected_case2 = {
+            "mon": [(time(12, 0), time(14, 0))],
+            "tue": [(time(9, 0), time(11, 0))],
+            "fri": [
+                (time(22, 0), time(23, 59, 59, 999999)),
+                (time(20, 0), time(22, 0)),
+            ],
+        }
+        self.assertEqual(self.fd_case2.parse_config_string(), expected_case2)
+
+        with self.assertRaises(AssertionError):
+            self.fd_case3.parse_config_string()
+
+        with self.assertRaises(AssertionError):
+            self.fd_case4.parse_config_string()
 
     # Test with the config string that allows/disallows free drinks on freezed time
     def test_can_get_free_drink(self):
